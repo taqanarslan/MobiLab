4f21ea18905ead2cca74cebedd98d01c74ac06a6
Atakan Arslan <ataarslan52@gmail.com>
2022-08-31 20:57:33 +0300

    update

--- Ödev1/GizemGoksu/hw1/lib/colorfultexts.dart
+++ Ödev1/GizemGoksu/hw1/lib/colorfultexts.dart
@@ -1,42 +1,41 @@
 import 'package:flutter/material.dart';
 
 List<Text> colorfultext = [
-  const Text(
-      ""), //colorfultext[0] olduğunda ekranda yazı gözükmesin diye 0.indexi "" yaptım.
+  const Text(""), //colorfultext[0] olduğunda ekranda yazı gözükmesin diye 0.indexi "" yaptım.
   const Text(
     'Mor',
-    style: TextStyle(color: Colors.purple),
+    style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 20),
   ),
   const Text(
     'Mavi',
-    style: TextStyle(color: Colors.blue),
+    style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 20),
   ),
   const Text(
     'Turuncu',
-    style: TextStyle(color: Colors.orange),
+    style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold, fontSize: 20),
   ),
   const Text(
     'Pembe',
-    style: TextStyle(color: Colors.pink),
+    style: TextStyle(color: Colors.pink, fontWeight: FontWeight.bold, fontSize: 20),
   ),
   const Text(
     'Sarı',
-    style: TextStyle(color: Colors.yellow),
+    style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold, fontSize: 20),
   ),
   const Text(
     'Kahverengi',
-    style: TextStyle(color: Colors.brown),
+    style: TextStyle(color: Colors.brown, fontWeight: FontWeight.bold, fontSize: 20),
   ),
   const Text(
     'Kırmızı',
-    style: TextStyle(color: Colors.red),
+    style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 20),
   ),
   const Text(
     'Yeşil',
-    style: TextStyle(color: Colors.green),
+    style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold, fontSize: 20),
   ),
   const Text(
     'Gri',
-    style: TextStyle(color: Colors.grey),
+    style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 20),
   ),
 ];
--- Ödev1/GizemGoksu/hw1/lib/mainpage.dart
+++ Ödev1/GizemGoksu/hw1/lib/mainpage.dart
@@ -17,18 +17,12 @@ class _MainPageState extends State<MainPage> {
       home: Scaffold(
         body: Center(
           child: Column(mainAxisSize: MainAxisSize.min, children: [
-            rowwithboxes(
-                Box(index: 1, renk: Colors.purple),
-                Box(index: 2, renk: Colors.blue),
-                Box(index: 3, renk: Colors.orange)),
-            rowwithboxes(
-                Box(index: 4, renk: Colors.pink),
-                Box(index: 5, renk: Colors.yellow),
-                Box(index: 6, renk: Colors.brown)),
-            rowwithboxes(
-                Box(index: 7, renk: Colors.red),
-                Box(index: 8, renk: Colors.green),
-                Box(index: 9, renk: Colors.grey)),
+            rowwithboxes(Box(index: 1, renk: Colors.purple), Box(index: 2, renk: Colors.blue), Box(index: 3, renk: Colors.orange)),
+            SizedBox(height: 5),
+            rowwithboxes(Box(index: 4, renk: Colors.pink), Box(index: 5, renk: Colors.yellow), Box(index: 6, renk: Colors.brown)),
+            SizedBox(height: 5),
+            rowwithboxes(Box(index: 7, renk: Colors.red), Box(index: 8, renk: Colors.green), Box(index: 9, renk: Colors.grey)),
+            SizedBox(height: 40),
             colorfultext[no],
           ]),
         ),
@@ -42,29 +36,26 @@ class _MainPageState extends State<MainPage> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         colorfulbox(box1.index, box1.renk),
+        SizedBox(width: 5),
         colorfulbox(box2.index, box2.renk),
+        SizedBox(width: 5),
         colorfulbox(box3.index, box3.renk),
       ],
     );
   }
 
-  ElevatedButton colorfulbox(int number, Color colour) {
-    return ElevatedButton(
-      onPressed: () {
+  InkWell colorfulbox(int number, Color colour) {
+    return InkWell(
+      onTap: () {
         setState(() {
           no = number;
           //buradaki no değişkenini colorfultext listesindeki yazılara ulaşmak için kullandım.Her yeni number geldiğinde no değişeceğinden ve stateful widget kullandığımızdan dolayı yazılar değişecek.
         });
       },
-      style: ButtonStyle(
-          minimumSize: MaterialStateProperty.all(Size.zero),
-          padding: MaterialStateProperty.all(EdgeInsets.zero),
-          shadowColor: MaterialStateProperty.all(Colors.transparent),
-          backgroundColor: MaterialStateProperty.all(Colors.transparent)),
       child: Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)), color: colour),
         width: 100,
         height: 100,
-        color: colour,
       ),
     );
   }
