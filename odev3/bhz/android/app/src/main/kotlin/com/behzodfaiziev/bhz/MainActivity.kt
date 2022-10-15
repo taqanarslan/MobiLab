0691e40dedb0ae9da99203ed4fa337b1250e51f3
Behzod Faiziev <63793311+behzodfaiziev@users.noreply.github.com>
2022-10-15 06:30:46 +0300

    commit

---
+++ odev3/bhz/android/app/src/main/kotlin/com/behzodfaiziev/bhz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.behzodfaiziev.bhz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ odev3/bhz/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'myapp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+ 
